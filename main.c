383db7069f391f2442ec9719399cda9bf1feac35
deathzombee <arfarfwegotcha@gmail.com>
2022-11-02 15:10:47 -0700

    we now check for that arg

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 
 int main(int argc, char *argv[]) {
     // hw3 - add if check for argc == 1 -- print err to user
-    // else continue 
+    // else continue
+    if (argc == 1) {
+        printf("Error: No input specified.\n");
+        return 1;
+    }
     int y = atoi(argv[1]);
     printf("Your Number: %d\n", y);
     printf("After foo add 8: %d\n",foo(y));
